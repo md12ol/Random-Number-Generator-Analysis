d4ead0a5fd88da536f18e931453b1bfb21f13663
Michael <md12ol@brocku.ca>
2018-04-06 22:38:09 -0400

    Began setting up the format of the project.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    /**
+     * Michael Dubé
+     * COSC 4P03 Final Project
+     *
+     * TODO: Complete this for main method
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    } // main
+
+}
